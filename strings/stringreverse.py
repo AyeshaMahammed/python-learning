63366157ad859b904cff944a34ff481029f9b5cc
AyeshaMahammed <mahammed.ayeshafarheen@gmail.com>
2025-04-20 13:54:07 +0530

    Adding comments in stringreverse.py

--- strings/stringreverse.py
+++ strings/stringreverse.py
@@ -1,3 +1,3 @@
-mystring = 'abcde'
-reversed_string = mystring[::-1]
-print(reversed_string)
+mystring = 'abcde' # declare a string
+reversed_string = mystring[::-1] # slice the string by specifying the start, end and step values; assign the output(reversed string) to another variable 
+print(reversed_string) # print the output
