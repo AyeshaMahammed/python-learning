fc551157560d4c774df7a26e5339e6efff945992
AyeshaMahammed <mahammed.ayeshafarheen@gmail.com>
2025-04-21 01:12:03 +0000

    2 line program to print even numbers using for loop and range in python

---
+++ loops/forloop_even_number.py
@@ -0,0 +1,12 @@
+for num in range(0,11,2):
+    print(num)
+# This code prints even numbers from 0 to 10 using a for loop.
+# The range function generates numbers from 0 to 10 with a step of 2, which results in even numbers.
+# The for loop iterates through these numbers and prints each one.
+# The output will be:
+# 0
+# 2
+# 4
+# 6
+# 8
+# 10
