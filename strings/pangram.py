49665936e22f7bb26762f28be3125581c876cda9
AyeshaMahammed <mahammed.ayeshafarheen@gmail.com>
2025-05-08 06:24:52 +0000

    This program checks if a given string is a pangram - not case sensitive and ignores punctuations, digits, new line characters, tabs, carriage returns, extra spaces

--- strings/pangram.py
+++ strings/pangram.py
@@ -1,8 +1,10 @@
 """
+
 This script checks if a given string is a pangram.
 A pangram is a sentence that contains every letter of the alphabet at least once.
 For example, "The quick brown fox jumps over the lazy dog" is a pangram.
 The script defines a function `is_pangram` that takes a string as input and returns True if it is a pangram, otherwise False.
+
 """
 import string
 def is_pangram(str, alphabet=string.ascii_lowercase):
